b50e6ef8f031cc3bbb7801893e567e43bd446e27
angelica <angelicavargascancimanse@gmail.com>
2025-11-28 11:04:13 -0500

    Inicio de proyecto

---
+++ view/partials/navbar.php
@@ -0,0 +1,93 @@
+<nav class="navbar navbar-header navbar-expand-lg" data-background-color="blue2">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
+                <!-- Quick Actions -->
+                <li class="nav-item dropdown hidden-caret">
+                    <a class="nav-link dropdown-toggle" href="#" id="quickActionsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-bolt"></i>
+                        <span class="notification">Quick Actions</span>
+                    </a>
+                    <ul class="dropdown-menu quick-actions animated fadeIn" aria-labelledby="quickActionsDropdown">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-plus"></i>
+                                <span>Nueva Actividad</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-chart-line"></i>
+                                <span>Reporte Rápido</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <span>Ver Mapa</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-tasks"></i>
+                                <span>Tareas Pendientes</span>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-cog"></i>
+                                <span>Configuración Rápida</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                
+                <!-- User Profile -->
+                <li class="nav-item dropdown hidden-caret">
+                    <a class="dropdown-toggle profile-pic" data-bs-toggle="dropdown" href="#" aria-expanded="false">
+                        <div class="avatar-sm">
+                            <img src="assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user animated fadeIn">
+                        <li>
+                            <div class="user-box">
+                                <div class="avatar-lg">
+                                    <img src="assets/img/profile.jpg" alt="image profile" class="avatar-img rounded">
+                                </div>
+                                <div class="u-text">
+                                    <h4>Nombre Usuario</h4>
+                                    <p class="text-muted">usuario@zoocriadero.com</p>
+                                    <a href="profile.html" class="btn btn-xs btn-secondary btn-sm">Ver Perfil</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user-cog"></i>
+                                Mi Cuenta
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-cog"></i>
+                                Configuración
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-question-circle"></i>
+                                Ayuda
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-power-off"></i>
+                                Cerrar Sesión
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ view/partials/sidebar.php
@@ -0,0 +1,203 @@
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+
+            <a href="index.html" class="logo">
+                <img src="assets/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20">
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+
+        </div>
+        <!-- End Logo Header -->	
+    </div>	
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item active">
+                    <a href="visualizacion-mapa.html">
+                        <i class="fas fa-map"></i>
+                        <p>Visualización de Mapa</p>
+                    </a>
+                </li>
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">Gestión del Sistema</h4>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#gestionUsuarios">
+                        <i class="fas fa-users"></i>
+                        <p>Gestión de Usuarios</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="gestionUsuarios">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="gestion-usuarios/lista-usuarios.html">
+                                    <span class="sub-item">Lista de Usuarios</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-usuarios/crear-usuario.html">
+                                    <span class="sub-item">Crear Usuario</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-usuarios/permisos.html">
+                                    <span class="sub-item">Permisos</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#gestionZoocriadero">
+                        <i class="fas fa-paw"></i>
+                        <p>Gestión de Zoocriadero</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="gestionZoocriadero">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="gestion-zoocriadero/informacion.html">
+                                    <span class="sub-item">Información General</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-zoocriadero/especies.html">
+                                    <span class="sub-item">Gestión de Especies</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-zoocriadero/instalaciones.html">
+                                    <span class="sub-item">Instalaciones</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#gestionTanques">
+                        <i class="fas fa-water"></i>
+                        <p>Gestión de Tanques</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="gestionTanques">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="gestion-tanques/lista-tanques.html">
+                                    <span class="sub-item">Lista de Tanques</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-tanques/crear-tanque.html">
+                                    <span class="sub-item">Crear Tanque</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-tanques/parametros.html">
+                                    <span class="sub-item">Parámetros</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#gestionActividades">
+                        <i class="fas fa-tasks"></i>
+                        <p>Gestión de Actividades</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="gestionActividades">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="gestion-actividades/calendario.html">
+                                    <span class="sub-item">Calendario</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-actividades/registro.html">
+                                    <span class="sub-item">Registro de Actividades</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="gestion-actividades/tipos.html">
+                                    <span class="sub-item">Tipos de Actividad</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#reportes">
+                        <i class="fas fa-chart-bar"></i>
+                        <p>Reportes</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="reportes">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="reportes/produccion.html">
+                                    <span class="sub-item">Reportes de Producción</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="reportes/salud.html">
+                                    <span class="sub-item">Reportes de Salud</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="reportes/actividades.html">
+                                    <span class="sub-item">Reportes de Actividades</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="reportes/personalizados.html">
+                                    <span class="sub-item">Reportes Personalizados</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#configuracion">
+                        <i class="fas fa-cogs"></i>
+                        <p>Configuración</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="configuracion">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="configuracion/general.html">
+                                    <span class="sub-item">Configuración General</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="configuracion/parametros-sistema.html">
+                                    <span class="sub-item">Parámetros del Sistema</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="configuracion/backup.html">
+                                    <span class="sub-item">Backup y Restauración</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
