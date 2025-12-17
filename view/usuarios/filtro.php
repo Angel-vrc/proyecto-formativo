1d327cefced041703aade908f5f519b42cd5bf5f
angelica <angelicavargascancimanse@gmail.com>
2025-12-16 20:50:26 -0500

    Implementacion mensaje de no registros en zoo, usuarios y roles

--- view/usuarios/filtro.php
+++ view/usuarios/filtro.php
@@ -1,40 +1,44 @@
 <?php
-    while($usuario = pg_fetch_assoc($usuarios)){
-        echo "<tr>";
-            echo "<td>".$usuario['id']."</td>";
-            echo "<td>".$usuario['nombre']."</td>";
-            echo "<td>".$usuario['apellido']."</td>";
-            echo "<td>".$usuario['telefono']."</td>";
-            echo "<td>".$usuario['rol_nombre']."</td>";
-            echo "<td>";
-                if (tienePermiso('usuarios', 'Consultar')) {
-                    echo "<button type='button' class='btn btn-info mx-2' onclick='abrirModalDetalles(this)'
-                        data-nombre='".$usuario['nombre']."'
-                        data-apellido='".$usuario['apellido']."'
-                        data-documento='".$usuario['documento']."'
-                        data-telefono='".$usuario['telefono']."'
-                        data-correo='".$usuario['correo']."'
-                        data-rol='".$usuario['rol_nombre']."'
-                        data-estado ='".$usuario['estado_nombre']."'>
-                        Ver Detalles
-                    </button>";
-                }
 
-                if (tienePermiso('usuarios', 'Actualizar')) {
-                    echo "<a href='".getUrl("Usuarios", "Usuario", "getUpdate", array("id"=>$usuario['id']))."' class='btn btn-primary mx-2'>Editar</a>";
-                }
+    if ($usuarios && pg_num_rows($usuarios) > 0) {
+        while($usuario = pg_fetch_assoc($usuarios)){
+            echo "<tr>";
+                echo "<td>".$usuario['id']."</td>";
+                echo "<td>".$usuario['nombre']."</td>";
+                echo "<td>".$usuario['apellido']."</td>";
+                echo "<td>".$usuario['telefono']."</td>";
+                echo "<td>".$usuario['rol_nombre']."</td>";
+                echo "<td>";
+                    if (tienePermiso('usuarios', 'Consultar')) {
+                        echo "<button type='button' class='btn btn-info mx-2' onclick='abrirModalDetalles(this)'
+                            data-nombre='".$usuario['nombre']."'
+                            data-apellido='".$usuario['apellido']."'
+                            data-documento='".$usuario['documento']."'
+                            data-telefono='".$usuario['telefono']."'
+                            data-correo='".$usuario['correo']."'
+                            data-rol='".$usuario['rol_nombre']."'
+                            data-estado ='".$usuario['estado_nombre']."'>
+                            Ver Detalles
+                        </button>";
+                    }
 
-                if ($usuario['id_estado'] == 1) {
-                    if (tienePermiso('usuarios', 'Eliminar')) {
-                        echo "<a href='".getUrl("Usuarios","Usuario","getDelete",array("id"=>$usuario['id']))."' class='btn btn-danger'>Eliminar</a>";
+                    if (tienePermiso('usuarios', 'Actualizar')) {
+                        echo "<a href='".getUrl("Usuarios", "Usuario", "getUpdate", array("id"=>$usuario['id']))."' class='btn btn-primary mx-2'>Editar</a>";
                     }
-                } elseif ($usuario['id_estado'] == 2) {
-                    if (tienePermiso('usuarios', 'Eliminar')) {
-                        echo "<a href='".getUrl("Usuarios","Usuario","updateStatus",array("id"=>$usuario['id']))."' class='btn btn-success'>Activar</a>";
-                    }
-                }                          
-            echo "</td>";
-        echo "</tr>";
+
+                    if ($usuario['id_estado'] == 1) {
+                        if (tienePermiso('usuarios', 'Eliminar')) {
+                            echo "<a href='".getUrl("Usuarios","Usuario","getDelete",array("id"=>$usuario['id']))."' class='btn btn-danger'>Eliminar</a>";
+                        }
+                    } elseif ($usuario['id_estado'] == 2) {
+                        if (tienePermiso('usuarios', 'Eliminar')) {
+                            echo "<a href='".getUrl("Usuarios","Usuario","updateStatus",array("id"=>$usuario['id']))."' class='btn btn-success'>Activar</a>";
+                        }
+                    }                          
+                echo "</td>";
+            echo "</tr>";
+        }
+    }else{
+        echo "<tr><td colspan='7' class='text-center'>No se encontraron registros</td></tr>";
     }
-    $c = pg_num_rows($usuarios);
 ?>
--- view/zoocriaderos/filtro.php
+++ view/zoocriaderos/filtro.php
@@ -1,43 +1,47 @@
 <?php
-    while($zoo = pg_fetch_assoc($zoocriaderos)){
-        echo "<tr>";
-            echo "<td>".$zoo['id_zoocriadero']."</td>";
-            echo "<td>".$zoo['nombre']."</td>";
-            echo "<td>".$zoo['direccion']."</td>";
-            echo "<td>".$zoo['barrio']."</td>";
-            echo "<td>".$zoo['nombre_responsable']." ".$zoo['apellido_responsable']."</td>";
-            echo "<td>";
-                if (tienePermiso('zoocriaderos', 'Consultar')) {
-                    echo "<button type='button' class='btn btn-info mx-2' onclick='abrirModalDetalles(this)'
-                        data-id='".$zoo['id_zoocriadero']."'
-                        data-nombre='".$zoo['nombre']."'
-                        data-comuna='".$zoo['comuna']."'
-                        data-barrio='".$zoo['barrio']."'
-                        data-direccion='".$zoo['direccion']."'
-                        data-coordenadas='".$zoo['coordenadas']."'
-                        data-responsable='".$zoo['nombre_responsable']."'
-                        data-telefono='".$zoo['telefono']."'
-                        data-correo='".$zoo['correo']."'
-                        data-estado='".$zoo['nombre_estado']."'>
-                        Ver Detalles
-                    </button>";
-                }
-
-                if (tienePermiso('zoocriaderos', 'Actualizar')) {
-                    echo "<a href='".getUrl("Zoocriaderos", "Zoocriadero", "getUpdate", array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-primary mx-2'>Editar</a>";
-                }
-
-                if ($zoo['id_estado'] == 1) {
-                    if (tienePermiso('zoocriaderos', 'Eliminar')) {
-                        echo "<a href='".getUrl("Zoocriaderos","Zoocriadero","getDelete",array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-danger'>Eliminar</a>";
+    if ($zoocriaderos && pg_num_rows($zoocriaderos) > 0) {
+        while($zoo = pg_fetch_assoc($zoocriaderos)){
+            echo "<tr>";
+                echo "<td>".$zoo['id_zoocriadero']."</td>";
+                echo "<td>".$zoo['nombre']."</td>";
+                echo "<td>".$zoo['direccion']."</td>";
+                echo "<td>".$zoo['barrio']."</td>";
+                echo "<td>".$zoo['nombre_responsable']." ".$zoo['apellido_responsable']."</td>";
+                echo "<td>";
+                    if (tienePermiso('zoocriaderos', 'Consultar')) {
+                        echo "<button type='button' class='btn btn-info mx-2' onclick='abrirModalDetalles(this)'
+                            data-id='".$zoo['id_zoocriadero']."'
+                            data-nombre='".$zoo['nombre']."'
+                            data-comuna='".$zoo['comuna']."'
+                            data-barrio='".$zoo['barrio']."'
+                            data-direccion='".$zoo['direccion']."'
+                            data-coordenadas='".$zoo['coordenadas']."'
+                            data-responsable='".$zoo['nombre_responsable']."'
+                            data-telefono='".$zoo['telefono']."'
+                            data-correo='".$zoo['correo']."'
+                            data-estado='".$zoo['nombre_estado']."'>
+                            Ver Detalles
+                        </button>";
                     }
-                } elseif ($zoo['id_estado'] == 2) {
-                    if (tienePermiso('zoocriaderos', 'Eliminar')) {
-                        echo "<a href='".getUrl("Zoocriaderos","Zoocriadero","updateStatus",array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-success'>Activar</a>";
+
+                    if (tienePermiso('zoocriaderos', 'Actualizar')) {
+                        echo "<a href='".getUrl("Zoocriaderos", "Zoocriadero", "getUpdate", array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-primary mx-2'>Editar</a>";
                     }
-                }                          
-            echo "</td>";
-        echo "</tr>";
+
+                    if ($zoo['id_estado'] == 1) {
+                        if (tienePermiso('zoocriaderos', 'Eliminar')) {
+                            echo "<a href='".getUrl("Zoocriaderos","Zoocriadero","getDelete",array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-danger'>Eliminar</a>";
+                        }
+                    } elseif ($zoo['id_estado'] == 2) {
+                        if (tienePermiso('zoocriaderos', 'Eliminar')) {
+                            echo "<a href='".getUrl("Zoocriaderos","Zoocriadero","updateStatus",array("id"=>$zoo['id_zoocriadero']))."' class='btn btn-success'>Activar</a>";
+                        }
+                    }                          
+                echo "</td>";
+            echo "</tr>";
+        }
+    }else{
+        echo "<tr><td colspan='6' class='text-center'>No se encontraron registros</td></tr>";
     }
     $c = pg_num_rows($zoocriaderos);
 ?>
